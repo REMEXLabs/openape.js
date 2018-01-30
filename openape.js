e90b2127ec052daa4a0e736697fbdea5fe5d5bff
Lukas84 <smirek@hdm-stuttgart.de>
2018-01-30 13:50:52 +0100

    debugging of constructor

--- openape.js
+++ openape.js
@@ -6,7 +6,11 @@
 var openAPE_API = {
 
 		tokenPath : "/token",
-		openApeServerUrl : "http://openape.gpii.eu"
+userContextPath : "api/user-contexts",
+	     taskContextPath : "/api/task-contexts",
+	     equipmentContextPath : "/api/equipment-contexts",
+	     environmentContextPath : "/api/envronment-contexts",
+	      		openApeServerUrl : "http://openape.gpii.eu"
 };
 
 var openAPE = {
@@ -26,7 +30,7 @@ var openAPE = {
 	createUser : function (username, email, password, serverurl) {
 
 		if(serverurl === undefined){
-			servrUrl = openAPE_API.openAPE.openApeServerUrl;
+			servrUrl = openAPE_API.openApeServerUrl;
 		} 
 
 		var objUser = new Object();
@@ -45,28 +49,51 @@ var openAPE = {
 	} // createUser
 }
 	class Client {
-		constructor(userName, password, serverUrl) {
-	        if(serverurl === undefined){
-	            this.serverUrl = serverUrl;
-	        } else if(serverurl === "/"){
+    	
+	constructor(username, password, serverUrl) {
+		this.	defaultContentType = "application/json";
+		this.token;
+		if(serverUrl === undefined){
+	            this.serverUrl =openAPE_API.openapeServerUrl;
+	        } else if(serverUrl === "/"){
 	            this.serverUrl =  window.location.protocol;
 	        } else {
-	            this.serverUrl = serverurl;
+	            this.serverUrl = serverUrl;
 	        }
 
-	        if(isPasswordCorrect(password) && isUsernameCorrect(userName)){
+	        if(isPasswordCorrect(password) && isUsernameCorrect(username)){
 	    		var data = "grant_type=password&username="+encodeURIComponent(username)+"&password="+encodeURIComponent(password);
-
-	        	var httpRequest = createHttpRequest("POST", openape.api.tokenPath, function(responseText){
+	    		
+console.log("data: " +data);
+	        	var httpRequest = this.createHttpRequest("POST", openAPE_API.tokenPath  , 
+	        			function(responseText){
 	        		this.token =JSON.parse(responseText).access_token; 
-	        			        	}, 'application/x-www-form-urlencoded'
+	        			        	}, undefined,   
+	        			        	'application/x-www-form-urlencoded'
  );
 	        	httpRequest.send(data);
 	        	}
 
 	        
 		}// constructor
+
+	
 		
+	   /* * getUserContextList
+	    * 
+	    * This function is used to retrieve a list of URIs to accessible
+	    * user contexts This Function relates to ISO/IEC 24752-8 7.2.6
+	    * 
+	    * @param {string}
+	    *            query - the query to filter the relevant contexts
+	    * @param{string} contentType - the used content-type
+	    * @return {object} - A javascript object with all status
+	    *         information
+	    */
+	   getUserContextList (successCallback, errorCallback, query, contentType) {
+		   return getContextList(openApe_API.userContextPath, successCallback, errorCallback, query, successCallback, errorCallbackcontentType);
+	   	}
+
 		createContext (path, context, successCallback, contentType) {
 			   if(isTokenCorrect() && isContextCorrect(context)){	
 	httpRequest = createHttpRequest("POST", 
@@ -122,21 +149,39 @@ getContextList(path, successCallback, query, contentType){
 		   }
 
 		
-		createHttpRequest(verb, path, successCallback, data, contentType) {
-			var request = new XMLHttpRequest();
+		createHttpRequest(verb, path, successCallback, errorCallback, contentType) {
+			let request = new XMLHttpRequest();
 			request.open(verb, this.serverUrl + path);
 			   if (this.token !== undefined) {
 				   request.setRequestHeader("authorisation, this.token");
 			   }
-			
-			
-			xmlHttp.onreadystatechange = function() { 
-		        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-		            successCallback(xmlHttp.responseText);
-		    }
+			   
+			   if(contentType == "application/json"  || contentType == "application/x-www-form-urlencoded"){
+			   console.log("contentType: " + contentType);
+				   request.setRequestHeader("Content-type", contentType);
+			   }else { 
+console.log("standard contentType");
+			   request.setRequestHeader("Content-type", this.defaultContentType);
+			}
+			   
+			   request.onload = function () {
+				   console.log(request.responseURL); // http://example.com/test
+				 };
 
-			
-			return request;
+			   
+			request.onreadystatechange = function() { 
+		        if (request.readyState == 4 && request.status == 200){
+		            successCallback(request.responseText);
+			} 
+		        else if (request.status == 404) {
+				console.log("Error: " + request.status );
+			} else {
+				console.log("readyState: " +request.readyState  );
+				console.log("HTTP: " +request.status  );
+				console.log("Error: " + request.responseText);
+			} 
+			};
+		   return request;
 			
 			}
 		
@@ -174,10 +219,35 @@ getContextList(path, successCallback, query, contentType){
 			   return isContextCorrect;	    	
 		   }
 
-		
+	
 	}// client
+
+function isPasswordCorrect(password) {
+   	var isPasswordCorrect = true;
+   	if(password==""){
+   		console.log("Password can not be empty");
+   		isPasswordCorrect = false;
+   	} else if(password === undefined){
+   		console.log("Please enter a password");
+   		isPasswordCorrect = false;
+   	}
+return isPasswordCorrect;
+   	
+}
 		
+function isUsernameCorrect(username) {
+   	var isUsernameCorrect = true;
+   	if(username == ""){
+   		arrStatusText.push("Username can not be empty");
+   		isUsernameCorrect = false;
+   	} else if(username === undefined){
+   		console.log("Please enter a username");
+   		isUsernameCorrect = false;
+   	}
+   	return isUsernameCorrect
+
+}
 
 
 console.log("starting");
-var myClient = new Client("daniel","ich","http://localhost:4567");
+//var myClient = new Client("daniel","ich","http://localhost:4567");
