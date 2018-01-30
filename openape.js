dbcbb908289e36f290c7ec8d7da3787c414de6e9
Lukas84 <smirek@hdm-stuttgart.de>
2018-01-30 07:36:53 +0100

    isUserNameCorrect,  is PasswordCorrect, isEMailCorrect functions added

--- openape.js
+++ openape.js
@@ -5,8 +5,8 @@
 */
 var openAPE_API = {
 
-		tokenPath : "/api/token",
-		openApeServerUrl = "http://openape.gpii.eu"
+		tokenPath : "/token",
+		openApeServerUrl : "http://openape.gpii.eu"
 };
 
 var openAPE = {
@@ -44,7 +44,7 @@ var openAPE = {
 
 	} // createUser
 }
-	class client {
+	class Client {
 		constructor(userName, password, serverUrl) {
 	        if(serverurl === undefined){
 	            this.serverUrl = serverUrl;
@@ -59,9 +59,9 @@ var openAPE = {
 
 	        	var httpRequest = createHttpRequest("POST", openape.api.tokenPath, function(responseText){
 	        		this.token =JSON.parse(responseText).access_token; 
-	        			        	}, data, 'application/x-www-form-urlencoded'
+	        			        	}, 'application/x-www-form-urlencoded'
  );
-	        	httpRequest.send();
+	        	httpRequest.send(data);
 	        	}
 
 	        
@@ -69,7 +69,7 @@ var openAPE = {
 		
 		createContext (path, context, successCallback, contentType) {
 			   if(isTokenCorrect() && isContextCorrect(context)){	
-	httpRequest = getHttpRequest("POST", 
+	httpRequest = createHttpRequest("POST", 
 	path, function(responseText){
 	callback(responseText);
 	}, context,
@@ -80,7 +80,7 @@ var openAPE = {
 		
 		getContext (path, contextId, successCallback, undefined, contentType) {
 				   if(isTokenCorrect() && isContextIdCorrect(contextId) ){
-				   let httpRequest= getHttpRequest("GET", path + "/" + contextId, function(responseText) {
+				   let httpRequest= createHttpRequest("GET", path + "/" + contextId, function(responseText) {
 				   successCallback(parse(response));
 				   }, contentType);
 				   httpRequest.send(null);
@@ -105,12 +105,31 @@ var openAPE = {
 			    }
 		   }
 
+getContextList(path, successCallback, query, contentType){
+			   httpRequest = createHttpRequest("GET",path, function(responseText){
+				   
+				successCallback(parse(responseText));   
+			   }, contentType);
+			   httpRequest.send(null);
+		   }
+		   
+		   parse(responseText, contentType){
+			   var result;
+			   if (contentType == "application/json"){
+				   result = JSON.parse(responseText);
+			   }
+			   return result;
+		   }
 
 		
 		createHttpRequest(verb, path, successCallback, data, contentType) {
 			var request = new XMLHttpRequest();
 			request.open(verb, this.serverUrl + path);
-		    
+			   if (this.token !== undefined) {
+				   request.setRequestHeader("authorisation, this.token");
+			   }
+			
+			
 			xmlHttp.onreadystatechange = function() { 
 		        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
 		            successCallback(xmlHttp.responseText);
@@ -120,6 +139,45 @@ var openAPE = {
 			return request;
 			
 			}
+		
+		isTokenCorrect(){
+			   let isTokenCorrect = true;
+	if (this.token === undefined){
+	console.log("Please initialize the library");
+				   isTokenCorrect = false;
+			   } 
+			   return isTokenCorrect;
+		   }	    	
+
+	isContextIdCorrect(contextId){
+			   let isContextIdCorrect = true;
+			   if(contextId==""){
+	console.log("The contextId can not be empty");
+				   isContextIdCorrect = false;
+			   } else if(contextId === undefined){
+	console.log("Please enter a contextId");
+				   isContextIdCorrect = false;
+			   }
+		    		    	
+			   return isContextIdCorrect;
+		   }
+		    	
+	isContextCorrect(context){
+			   var isContextCorrect = true;
+			   if(context==""){
+				   arrStatusText.push("The context can not be empty");
+				   isContextCorrect = false;
+			   } else if(context === undefined){
+				   arrStatusText.push("Please enter a context");
+				   isContextCorrect = false;
+			   }
+			   return isContextCorrect;	    	
+		   }
+
+		
 	}// client
 		
 
+
+console.log("starting");
+var myClient = new Client("daniel","ich","http://localhost:4567");
